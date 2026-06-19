efd067a45e090480053db07a1e175bd3d07b12cb
manishprakash2521 <manishpandian1998@gmail.com>
2026-06-19 17:11:11 +0530

    Update EmployeeController.java

--- EmployeeManagementSystem/controller/EmployeeController.java
+++ EmployeeManagementSystem/controller/EmployeeController.java
@@ -3,10 +3,13 @@ package com.example.EmployeeManagementSystem.controller;
 import com.example.EmployeeManagementSystem.dto.EmployeeDto;
 import com.example.EmployeeManagementSystem.entity.Employee;
 import com.example.EmployeeManagementSystem.service.EmployeeService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -15,17 +18,27 @@ public class EmployeeController {
 
     private EmployeeService employeeService;
 
-    // Build Add employee
-
     @PostMapping("/add")
-    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
+    public ResponseEntity<EmployeeDto> createEmployee(@Valid @RequestBody EmployeeDto employeeDto){
         EmployeeDto savedEmployee=employeeService.createEmployee(employeeDto);
         return new ResponseEntity<>(savedEmployee,HttpStatus.CREATED);
     }
 
     @GetMapping({"id"})
-    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long Id){
-        EmployeeDto employeeDto=employeeService.getEmployeeById(Id);
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long id){
+        EmployeeDto employeeDto=employeeService.getEmployeeById(id);
         return  ResponseEntity.ok(employeeDto);
+    }
+
+    @GetMapping("/allEmployees")
+    public ResponseEntity <List<EmployeeDto>> getAllEmployee(){
+        List<EmployeeDto> allEmployeeDto=employeeService.getAllEmployee();
+        return ResponseEntity.ok(allEmployeeDto);
+    }
+
+    @GetMapping("/{id}/salary")
+    public ResponseEntity<EmployeeDto> getEmployeeSalary(@PathVariable("id") Long id){
+        EmployeeDto employeeSalaryDto=employeeService.getEmployeeSalary(id);
+        return ResponseEntity.ok(employeeSalaryDto);
     }
 }
