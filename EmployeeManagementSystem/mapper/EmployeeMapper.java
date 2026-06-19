f390203dfc3384a5146037d8233546f5ad9d59b3
manishprakash2521 <manishpandian1998@gmail.com>
2026-06-19 17:12:26 +0530

    Update EmployeeMapper.java

--- EmployeeManagementSystem/mapper/EmployeeMapper.java
+++ EmployeeManagementSystem/mapper/EmployeeMapper.java
@@ -5,11 +5,11 @@ import com.example.EmployeeManagementSystem.entity.Employee;
 
 public class EmployeeMapper {
 
-    public static EmployeeDto mapToEmployeeDto(Employee employee){
-        return new EmployeeDto(employee.getId(), employee.getFirstName(), employee.getLastName(),employee.getEmail());
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(employee.getId(), employee.getFirstName(), employee.getLastName(), employee.getEmail(), employee.getSalary());
     }
 
-    public static Employee mapToEmployee(EmployeeDto employeeDto){
-        return new Employee(employeeDto.getId(), employeeDto.getFirstName(), employeeDto.getLastName(), employeeDto.getEmail());
+    public static Employee mapToEmployee(EmployeeDto employeeDto) {
+        return new Employee(employeeDto.getId(), employeeDto.getFirstName(), employeeDto.getLastName(), employeeDto.getEmail(), employeeDto.getSalary());
     }
 }
